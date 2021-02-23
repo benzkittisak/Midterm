ac27e4c62fffb3ec51fb118acc241c6be96363f2
“benzkittisak” <6206021621082@fitm.kmutnb.ac.th>
2021-02-24 00:11:12 +0700

    แบบฝึก 5 ข้อแรก

---
+++ 2/star.js
@@ -0,0 +1,17 @@
+function starRating(data){
+    const sum = data.reduce((a,b)=>{
+        return a+b;
+    })
+    let vote = 0;
+    for(let i =0 ; i<data.length;i++){
+        vote += data[i] * (i+1);
+    }
+    let avg = vote / sum ;
+    
+    return `[${avg.toFixed(2)}] ${'*'.repeat(Math.round(avg))}`;
+
+}
+
+console.log(starRating([55, 67, 98, 115, 61]));
+console.log(starRating([0, 2, 0, 1, 23]));
+console.log(starRating([16, 17, 23, 40, 45]));
