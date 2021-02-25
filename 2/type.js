883ea2cc7aef291c09f75c80c7854739f2def124
“benzkittisak” <6206021621082@fitm.kmutnb.ac.th>
2021-02-25 17:05:47 +0700

    อัพเพิ่มข้อ 6 7 8

---
+++ 2/type.js
@@ -0,0 +1,13 @@
+function arrayValuesTypes(){
+    let arr = arguments[0];
+    let result = [];
+    for(let value of arr){
+        result.push(typeof value);
+    }
+    return result;
+}
+
+
+console.log(arrayValuesTypes([1, 2, "null", []]));
+console.log(arrayValuesTypes(["214", true, false, 2, 2.15, [], null]));
+console.log(arrayValuesTypes([21.1, "float", "array", ["I am array"], null, true, 214]));
